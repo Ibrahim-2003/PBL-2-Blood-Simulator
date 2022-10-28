bdc96c4708a39da532a3f1727a4c847dd048d4e9
atian722 <108018979+atian722@users.noreply.github.com>
2022-10-28 09:16:17 -0500

    Add files via upload
    Set up the code

---
+++ bloodModel.m
@@ -0,0 +1,21 @@
+% PBL Team 1, BIOE 252, Fall 2022, PBL 2: Modeling Blood Flow
+% bloodModel.m
+% Models blood flow through the human body in different conditions
+% Last modified: 10/28/22
+
+function bloodModel
+    %the driver
+    init = %initial conditions;
+    organ1(init);
+    organ2(organ1out);
+end
+
+function organ1()
+
+
+end
+
+function organ2()
+
+
+end
