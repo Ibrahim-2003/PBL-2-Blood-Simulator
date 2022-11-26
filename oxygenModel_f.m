f4064ef8cd3c2614ee14275c26f0ec1cd270e3f0
atian722 <108018979+atian722@users.noreply.github.com>
2022-11-25 23:33:28 -0600

    Add files via upload

--- oxygenModel_f.m
+++ oxygenModel_f.m
@@ -6,8 +6,9 @@
 function oxygenModel_f
     %the driver
     %setting up initial values, need to compare this with the flow model
-    tspan = 0:0.0001:15;
-    y0 = 25.935;
+    tspan = 0:0.01:2497;
+    %y0 = 0.195; %initial mL of O2
+    y0 = 0.0068; %mmol/mL
 
     %running the ode
     [t,y] = ode45(@heartoxygencons,tspan,y0);
@@ -16,27 +17,50 @@ function oxygenModel_f
 
     %check Q_left_heart
     load("Q_left_heart.mat");
+    t = 0:0.01:2500;
     plot(t,Q_left_heart)
 end
 
 function dy = heartoxygencons(t,y)
-    %an ode modeling oxygen concentration in the left heart
-    vol = 133; %mL
-    oconca = 0.195*vol;
-    oconcv = 0.145*vol;
-
-    weight = 150; %you should probably check these too
-    height = 180;
-    age = 21;
-    OP0 = 88.362 + 13.397*weight + 4.799*height - 5.677*age; %BMR
-    
+    %an ode modeling oxygen volume in the left heart
+    vol = 214; %mL
+    %oconca = 0.195; %mL O2/mL blood
+    oconca = 0.017; %mmol/mL
+    oconcv = 0.145;
+    oconcavg = 0.17;
+  
     %values to input in ode
-    OP = -0.3; %consumption rate
+    %OP = -0.43; %consumption rate mL/sec
+    OP = -0.019; %consumption rate mmol/sec
     load("Q_left_heart.mat"); %this is from simulink
 
-    Qdot = Q_left_heart(round(t/0.0001,0)+1); %flow rate
-    sigmaO2 = (oconca - (OP0/Qdot))/25.935; %partition coefficient?
+    Qdot = Q_left_heart(round(t/0.01,0)+3)*0.05; %flow rate
+    %sigmaO2 = (oconca - (OP/250))/oconcavg;
+    %sigmaO2 = oconcv/oconcavg;
+    sigmaO2 = 0.3; %lol we figured this out
 
     %the actual ode
-    dy = (OP + Qdot.*(oconca-sigmaO2.*y))./331;
+    dy = (OP + Qdot.*(oconca-sigmaO2.*y))./vol;
+    %disp(Qdot.*(oconca-sigmaO2.*y));
+end
+
+function dy = simplified(t,y)
+    %an ode modeling oxygen volume in the left heart
+    vol = 214; %mL
+    oconca = 0.195; %mL O2/mL blood
+    oconcv = 0.145;
+    oconcavg = 0.17;
+
+    %values to input in ode
+    %OP = -0.43; %consumption rate mL/min
+    load("Q_left_heart.mat"); %this is from simulink
+
+    Qdot = Q_left_heart(round(t/0.01,0)+3)*0.05; %flow rate
+    OP = mean(Q_left_heart(round(20/0.001,0)+3:end));
+    %sigmaO2 = (oconca - (OP/250))/oconcavg;
+    %sigmaO2 = oconcv/oconcavg;
+    sigmaO2 = 0.3; %lol we figured this out
+
+    %the actual ode
+    dy = (OP + Qdot.*(oconca-oconcv))./vol;
 end
